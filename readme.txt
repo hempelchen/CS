f43e30a17e6f623282c06569fa7060fc01aa18b5
hempelchen <zhuce79@sina.com>
2014-06-03 11:12:52 +0800

    添加服务端模块

---
+++ readme.txt
@@ -0,0 +1,2 @@
+1.Server端，建议采用Eclipse加载，并自建 Tomcat 的web服务来加载 API 等 servlet；
+2.Client端，请使用 https://github.com/hempelchen/helloworld 里的 “Get/Post 测试”功能来验证
---
+++ server/Common/src/com/chb/api/Common.java
@@ -0,0 +1,81 @@
+package com.chb.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class API
+ */
+public class Common extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static String postType = "";
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Common() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter returnHTML;
+		readForm(request);
+		response.setContentType("text/html;charset=utf-8");
+		returnHTML = response.getWriter();
+		returnHTML.println("<html><head><title>");
+		returnHTML.println("API Common servlet (Get)");
+		returnHTML.println("</title></head><body>");
+		returnHTML.println("<h2> Welcome from Get! Greetings," + "this is you servlet answering.</h2></body></html>");
+		returnHTML.close();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter returnHTML;
+		returnHTML = response.getWriter();
+		readForm(request);
+		if( postType.equals("html")) {
+			response.setContentType("text/html;charset=utf-8");
+			returnHTML.println("<html><head><title>");
+			returnHTML.println("API Common servlet (Post)");
+			returnHTML.println("</title></head><body>");
+			returnHTML.println("<h2>Welcome from Post! Greetings," + "this is you servlet answering.</h2></body></html>");		
+		} else if (postType.equals("json")){
+			response.setContentType("application/json");
+			String jsonTest="[{\"Id\":\"103\",\"number\":\"790319\"},{\"Id\":\"104\",\"number\":\"830426\"}]";
+			returnHTML.println(jsonTest);
+		}
+		returnHTML.close();
+	}
+	
+    /** 
+     * 获取表单参数 
+     */  
+    private void readForm(HttpServletRequest request) {  
+        // TODO Auto-generated method stub  
+        Enumeration e = request.getParameterNames();  
+        while (e.hasMoreElements()) {  
+            String parameterName = (String) e.nextElement();  
+            String parameterValue= request.getParameter(parameterName);  
+//            request.setAttribute(parameterName, parameterValue);  
+            if(parameterName.equals("type")) {
+            	postType = parameterValue;
+            }
+        }  
+          
+    }  
+
+}
